ae639106edaec977446cf38edd76b5b56c779d69
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2025-07-01 19:57:52 -0400

    update

--- Menu.sh
+++ Menu.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+# This script is used to display a menu and execute the selected option.
+dialog --mgsbox "Welcome to the Menu" 10 30
+dialog --menu "Select an option" 15 50 4 \
+1 "Generer mot de passe" \
+2 "Telecharger wordlists(password)" \
+3 "Telecharger" \
+4 "Exit" 2>temp.txt
+choice=$(<temp.txt)
+rm temp.txt
+case $choice in
+1)  bash generator.sh ;;
+2)  
