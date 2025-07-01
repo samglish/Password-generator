800caa346ff5a51436f08603583160f70984e528
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2025-07-01 19:19:52 -0400

    update

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 dialog --msgbox "Bienvenue sur le generateur de mot de passe \n Veuillez Accepter pour continuer" 10 49
+min=$(dialog --inputbox "Entrez la longueur minimale du mot de passe" 10 49 2>&1 >/dev/tty)
+max=$(dialog --inputbox "Entrez la longueur maximale du mot de passe" 10 49 2>&1 >/dev/tty)
