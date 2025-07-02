dee7381576aa9080f6f2a2ca566362ba70b7e372
BEIDI DINA SAMUEL <77502731+samglish@users.noreply.github.com>
2025-07-01 20:02:22 -0400

    update

--- Menu.sh
+++ Menu.sh
@@ -4,10 +4,12 @@ dialog --mgsbox "Welcome to the Menu" 10 30
 dialog --menu "Select an option" 15 50 4 \
 1 "Generer mot de passe" \
 2 "Telecharger wordlists(password)" \
-3 "Telecharger wordlists en local" \
-4 "Exit" 2>temp.txt
+3 "Exit" 2>temp.txt
 choice=$(<temp.txt)
 rm temp.txt
 case $choice in
 1)  bash generator.sh ;;
-2)  
+2) wget https://raw.githubusercontent.com/blackhatscripts/wordlists/master/passwords.txt ;;
+3) exit 0 ;;
+*) dialog --msgbox "Invalid option selected" 10 30 ;;
+esac    
